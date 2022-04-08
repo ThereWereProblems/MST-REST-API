273d8438bb5bb8e2405d043b2ed2ecaaa508cbca
Dawid <dawid.gredzinski24@gmail.com>
2022-04-08 14:48:31 +0200

    init2

---
+++ MST-REST-API/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MST_REST_API.Models;
+using System.Diagnostics;
+
+namespace MST_REST_API.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpPost]
+        public IActionResult Index([FromQuery] string name)
+        {
+            return Ok("Hello " + name);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
